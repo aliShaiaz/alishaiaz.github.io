4c6feaf81de1c770ffdee291c4400b96ded39abb
alishaiaz <shaiazali@gmail.com>
2023-01-05 17:16:26 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./Components/CSS/App.css";
+
+import BasicPage from "./Components/Pages/basicPage";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <React.Fragment>
+      <BasicPage />
+    </React.Fragment>
   );
 }
 
---
+++ src/Components/Pages/basicPage.jsx
@@ -0,0 +1,33 @@
+import React from 'react';
+
+const BasicPage = () => {
+    return (
+        <div className='basicPage'>
+            <h1>Demo Page of Shaiaz Ali</h1>
+            <p>Description: This is a demo page, created by Shaiaz Ali.</p>
+            <p>
+                Shaiaz Ali <br />
+                Bachelor of Computer Science <br />
+                American International University - Bangladesh <br />
+                <br />
+                Email: contact@alishaiaz.me <br />
+                FB: <a href="https://facebook.com/alishaiaz">facebook.com/alishaiaz</a><br />
+                LinkedIn:<a href="https://www.linkedin.com/in/alishaiaz/" >linkedin.com/in/alishaiaz</a>
+                <br />
+                Github: <a href="https://github.com/alishaiaz">github.com/alishaiaz</a>
+                <br />
+            </p>
+            <div>
+                <h2>Porfolio:</h2>
+                <div>
+                    <a href="https://www.alishaiaz.me/dec"
+                    >Electronic Devices Project Website (Group 7)</a
+                    >
+                </div>
+            </div>
+
+        </div>
+    );
+}
+
+export default BasicPage;
